843330aa7e08d4c50dcb2fd3c183562a236281b0
Smikey Bros <smikeywork@gmail.com>
2026-05-13 00:19:28 +1000

    :heavy_plus_sign: Add compose navigation event library for Back handler in List-Detail Pane Layout

--- feature/chat/presentation/build.gradle.kts
+++ feature/chat/presentation/build.gradle.kts
@@ -25,6 +25,7 @@ kotlin {
                 implementation(libs.material3.adaptive)
                 implementation(libs.material3.adaptive.layout)
                 implementation(libs.material3.adaptive.navigation)
+                implementation(libs.jetbrains.navigation.event.compose)
                 implementation(libs.jetbrains.compose.backhandler)
                 implementation(libs.kotlinx.datetime)
 
