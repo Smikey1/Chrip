1c6750a02d05424a6d696eaff9dda51beb88bd4f
Smikey Bros <smikeywork@gmail.com>
2026-05-16 09:35:27 +1000

    :lipstick: Created Chat Message List Item UI Component

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/twugteam/admin/chat/presentation/chat_detail/component/MessageListItemUi.kt
@@ -0,0 +1,107 @@
+package com.twugteam.admin.chat.presentation.chat_detail.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.twugteam.admin.chat.domain.models.ChatMessageDeliveryStatus
+import com.twugteam.admin.chat.presentation.model.MessageUi
+import com.twugteam.admin.core.designsystem.components.avatar.ChatParticipantUi
+import com.twugteam.admin.core.designsystem.theme.ChirpTheme
+import com.twugteam.admin.core.designsystem.theme.extended
+import com.twugteam.admin.core.presentation.util.UiText
+
+@Composable
+fun MessageListItemUi(
+    messageUi: MessageUi,
+    onMessageLongClick: (MessageUi.LocalUserMessage) -> Unit,
+    onDismissMessageMenu: () -> Unit,
+    onDeleteClick: () -> Unit,
+    onRetryClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+    ) {
+        when (messageUi) {
+            is MessageUi.DateSeparator -> {
+                DateSeparator(
+                    date = messageUi.date.asString(),
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
+            is MessageUi.LocalUserMessage -> {
+                LocalUserMessage(
+                    message = messageUi,
+                    onMessageLongClick = onMessageLongClick,
+                    onDismissMessageMenu = onDismissMessageMenu,
+                    onDeleteClick = onDeleteClick,
+                    onRetryClick = onRetryClick
+                )
+            }
+
+            is MessageUi.OtherUserMessage -> {
+                OtherUserMessage(
+                    message = messageUi
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun DateSeparator(
+    date: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        HorizontalDivider(modifier = Modifier.weight(1f))
+        Text(
+            text = date,
+            modifier = Modifier.padding(horizontal = 40.dp),
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.extended.textPlaceholder
+        )
+        HorizontalDivider(modifier = Modifier.weight(1f))
+    }
+}
+
+
+@Preview
+@Composable
+private fun MessageListItemUiPreview() {
+    ChirpTheme(
+        isDarkTheme = true
+    ) {
+        MessageListItemUi(
+            modifier = Modifier.fillMaxSize(),
+            onMessageLongClick = {},
+            onRetryClick = {},
+            onDeleteClick = {},
+            onDismissMessageMenu = {},
+            messageUi = MessageUi.LocalUserMessage(
+                id = "121",
+                content = "Hey",
+                isMenuOpen = false,
+                formattedSentTime = UiText.DynamicString("now"),
+                deliveryStatus = ChatMessageDeliveryStatus.FAILED
+            )
+        )
+
+    }
+
+}
---
+++ feature/chat/presentation/src/commonMain/kotlin/com/twugteam/admin/chat/presentation/chat_detail/component/OtherUserMessage.kt
@@ -0,0 +1,62 @@
+package com.twugteam.admin.chat.presentation.chat_detail.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.twugteam.admin.chat.presentation.model.MessageUi
+import com.twugteam.admin.core.designsystem.components.avatar.ChatParticipantUi
+import com.twugteam.admin.core.designsystem.components.avatar.ChripAvatarPhoto
+import com.twugteam.admin.core.designsystem.components.chat.ChirpChatBubble
+import com.twugteam.admin.core.designsystem.components.chat.TrianglePosition
+import com.twugteam.admin.core.designsystem.theme.ChirpTheme
+import com.twugteam.admin.core.presentation.util.UiText
+
+@Composable
+fun OtherUserMessage(
+    message: MessageUi.OtherUserMessage,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        ChripAvatarPhoto(
+            displayInitialText = message.sender.initial,
+            imageUrl = message.sender.imageUrl
+        )
+        ChirpChatBubble(
+            messageContent = message.content,
+            sender = message.sender.username,
+            formattedDateTime = message.formattedSentTime.asString(),
+            trianglePosition = TrianglePosition.LEFT
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun OtherUserMessagePreview() {
+    ChirpTheme(
+        isDarkTheme = true
+    ) {
+        OtherUserMessage(
+            modifier = Modifier.fillMaxWidth(),
+            message = MessageUi.OtherUserMessage(
+                id = "121",
+                content = "Hey",
+                formattedSentTime = UiText.DynamicString("now"),
+                sender = ChatParticipantUi(
+                    userId = "343",
+                    username = "Kiran"
+                )
+            )
+        )
+    }
+}
