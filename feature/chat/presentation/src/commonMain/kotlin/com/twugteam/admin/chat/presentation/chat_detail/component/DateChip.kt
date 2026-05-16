203ceb47b86435018da2ce7cba216598489a3ad7
Smikey Bros <smikeywork@gmail.com>
2026-05-16 10:50:26 +1000

    :lipstick: Created Overflow Date Chip UI Component

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/twugteam/admin/chat/presentation/chat_detail/component/DateChip.kt
@@ -0,0 +1,61 @@
+package com.twugteam.admin.chat.presentation.chat_detail.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.twugteam.admin.core.designsystem.theme.ChirpTheme
+import com.twugteam.admin.core.designsystem.theme.extended
+
+@Composable
+fun DateChip(
+    date: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(100))
+            .background(MaterialTheme.colorScheme.surface)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.outline,
+                shape = RoundedCornerShape(100)
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = date,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 12.dp),
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.extended.textPlaceholder
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun DateChipPreview() {
+
+    ChirpTheme(
+        isDarkTheme = true
+    ) {
+        DateChip(
+            date = "Friday, 12:22 Pm",
+            modifier = Modifier.wrapContentSize()
+        )
+
+    }
+
+}
